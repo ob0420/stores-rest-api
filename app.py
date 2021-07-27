6944af683042542f345d3b9022fbaf6c5dbe1e6d
ob0420 <ogulgerek.babakulyyeva@bil.omu.edu.tr>
2021-07-28 01:17:05 +0300

    fff

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ from resources.item import Item, ItemList
 from resources.stores import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = ('uri', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'olya'
 api = Api(app)
